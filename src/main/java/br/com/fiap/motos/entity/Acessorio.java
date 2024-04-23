f84794a1b01f333550c8ab5c17c1360f54601edd
cauarojas <cauarojas@hotmail.com>
2024-04-23 08:42:05 -0300

    finished entities

--- src/main/java/br/com/fiap/motos/entity/Acessorio.java
+++ src/main/java/br/com/fiap/motos/entity/Acessorio.java
@@ -1,5 +1,6 @@
 package br.com.fiap.motos.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +11,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 
-
+@Entity
+@Table(name = "TB_CP2_ACESSORIO")
 public class Acessorio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ACESSORIO")
+    @SequenceGenerator(name = "SQ_ACESSORIO", sequenceName = "SQ_ACESSORIO", allocationSize = 1)
+    @Column(name = "ID_ACESSORIO")
     private Long id;
 
+    @Column(name = "NM_ACESSORIO", length = 30)
     private String nome;
 
+    @Column(name = "VL_ACESSORIO")
     private Double preco;
 
 }
--- src/main/java/br/com/fiap/motos/entity/Fabricante.java
+++ src/main/java/br/com/fiap/motos/entity/Fabricante.java
@@ -12,15 +12,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 
-
+@Entity
+@Table(name = "TB_CP2_FABRICANTE")
 public class Fabricante {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_FABRICANTE")
+    @SequenceGenerator(name = "SQ_FABRICANTE", sequenceName = "SQ_FABRICANTE", allocationSize = 1)
+    @Column(name = "ID_FABRICANTE")
     private Long id;
 
+    @Column(name = "NM_FABRICANTE")
     private String nome;
 
+    @Column(name = "NM_FANTASIA")
     private String nomeFantasia;
-
-
 
 }
